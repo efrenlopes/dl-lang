f69f178f9c317b5b3629854882acfbe8251d7174
efrenlopes <efrenlopes@gmail.com>
2026-01-21 14:43:08 -0300

    Implementados os nós da árvore sintática em nodes.py

---
+++ src/dl/tree/nodes.py
@@ -0,0 +1,194 @@
+from abc import ABC, abstractmethod
+from dl.lex.lexer import Token
+from dl.tree.visitor import Visitor
+
+
+class Node(ABC):
+    
+    def __init__(self, token: Token):
+        self.token = token
+    
+    @property
+    def line(self):
+        return self.token.line
+    
+    @abstractmethod
+    def accept(self, visitor: Visitor):
+        pass
+    
+    def __iter__(self):
+        children = []
+        for _, var in vars(self).items():
+            if isinstance(var, Node):
+                children.append(var)
+        return iter(children)
+
+
+
+class ExprNode(Node):
+    def __init__(self, token: Token):
+        super().__init__(token)
+        self.type = None
+
+
+
+class VarNode(ExprNode):
+    def __init__(self, token: Token):
+        super().__init__(token)
+        self.scope = None
+        
+    @property
+    def name(self):
+        return self.token.lexeme
+        
+    def accept(self, visitor: Visitor):
+        return visitor.visit_var_node(self)
+    
+    def __str__(self):
+        if self.type:
+            return f'{self.name}:{self.type}'
+        return self.name
+
+
+
+class LiteralNode(ExprNode):
+    def __init__(self, token: Token):
+        super().__init__(token)
+        self.value = None
+
+    @property
+    def raw_value(self):
+        return self.token.lexeme
+    
+    def accept(self, visitor: Visitor):
+        return visitor.visit_literal_node(self)
+    
+    def __str__(self):
+        if self.type:
+            return f'{self.value}:{self.type}'
+        return self.raw_value
+
+
+
+class BinaryNode(ExprNode):
+    def __init__(self, token: Token, expr1: ExprNode, expr2: ExprNode):
+        super().__init__(token)
+        self.expr1 = expr1
+        self.expr2 = expr2
+
+    @property
+    def operator(self):
+        return self.token.lexeme
+    
+    def accept(self, visitor: Visitor):
+        return visitor.visit_binary_node(self)
+
+    def __str__(self):
+        if self.type:
+            return f'{self.operator}:{self.type}'
+        return self.operator
+
+
+
+class ConvertNode(ExprNode):
+    def __init__(self, expr: ExprNode):
+        super().__init__(None)
+        self.expr = expr
+
+    @property
+    def operator(self):
+        if self.type:
+            return f'convert:{self.type}'        
+        return 'convert'
+    
+    def accept(self, visitor: Visitor):
+        return visitor.visit_convert_node(self)
+
+    def __str__(self):
+        return self.operator
+
+
+
+
+class StmtNode(Node):
+    
+    def __init__(self, token: Token):
+        super().__init__(token)
+        
+    def __str__(self):
+        return self.__class__.__name__
+
+
+
+
+class ProgramNode(StmtNode):
+    def __init__(self, token: Token, name: str, stmt: StmtNode):
+        super().__init__(token)
+        self.name = name
+        self.stmt = stmt
+        
+    def accept(self, visitor: Visitor):
+        return visitor.visit_program_node(self)
+
+
+
+
+class BlockNode(StmtNode):
+    def __init__(self, token: Token):
+        super().__init__(token)
+        self.stmts = []
+
+    def add_stmt(self, stmt: StmtNode):
+        self.stmts.append(stmt)
+
+    def accept(self, visitor: Visitor):
+        return visitor.visit_block_node(self)
+
+    def __iter__(self):
+        return iter(self.stmts)
+
+
+
+
+class DeclNode(StmtNode):
+    def __init__(self, token: Token, var: VarNode):
+        super().__init__(token)
+        self.var = var
+        
+    def accept(self, visitor: Visitor):
+        return visitor.visit_decl_node(self)
+
+
+
+
+class AssignNode(StmtNode):
+    def __init__(self, token: Token, var: VarNode, expr: ExprNode):
+        super().__init__(token)
+        self.var = var
+        self.expr = expr
+
+    def accept(self, visitor: Visitor):
+        return visitor.visit_assign_node(self)
+
+
+
+
+class IfNode(StmtNode):    
+    def __init__(self, token: Token, expr: ExprNode, stmt: StmtNode):
+        super().__init__(token)
+        self.expr = expr
+        self.stmt = stmt
+
+    def accept(self, visitor: Visitor):
+        return visitor.visit_if_node(self)
+
+
+
+
+class WriteNode(StmtNode):
+    def __init__(self, token: Token, expr: ExprNode):
+        super().__init__(token)
+        self.expr = expr
+
+    def accept(self, visitor: Visitor):
+        return visitor.visit_write_node(self)
